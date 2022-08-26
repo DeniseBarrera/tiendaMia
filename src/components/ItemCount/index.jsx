71b9b767f33b46aebb5cc0ab5939ea9b9d007d80
Denise Barrera <denise.barrera@outlook.com.ar>
2022-08-26 01:39:12 -0300

    ItemCount

---
+++ src/components/ItemCount/index.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import { HStack, Button, Text } from '@chakra-ui/react'
+
+const ItemCount = ({ initial, stock, onAdd }) => {
+
+    const [count, setCount] = useState(initial)
+    const increase = () => count < stock && setCount(count + 1)
+    const dicrease = () => count > initial && setCount(count - 1)    
+
+    return(
+        <HStack>
+            <Button variant='ghost' colorScheme='green' size='sm' onClick={dicrease}>-</Button>
+            <Text>{count}</Text>
+            <Button variant='ghost' colorScheme='green' size='sm' onClick={increase}>+</Button>
+            <Button colorScheme='green' size='sm'>Agregar</Button>
+        </HStack>
+    )
+}
+
+export { ItemCount }
